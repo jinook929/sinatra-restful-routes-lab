519539e2bdebbc94c5e10e0f3320b7fd95e8df0e
jinook929 <jinook929@gmail.com>
2021-02-24 22:42:33 -0500

    commit tree 5e5943deb913a48b3aae31d232646b30583a73d1

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Recipe.create(name: "Bomb.com Mac and Cheese", ingredients: "milk, butter, cheese, elbow pasta", cook_time: "20 minutes")
+Recipe.create(name: "Waldorf salad", ingredients: "apples, cabbage, oil, vinegar", cook_time: "10 minutes")
+Recipe.create(name: "Enchiladas con Salsa Verde", ingredients: "Tortillas, Queso Blanco, Tomatillos, Onion, Garlic, Black beans, Cilantro", cook_time: "20 minutes")
+Recipe.create(name: "Pumpkin pie", ingredients: "pumpkin, flour, butter, sugar", cook_time: "1 hour")
